81520ede3465ed2d3d584be7b86561e50f52ac1c
Nikolay Yordanov <me@nyordanov.com>
2013-03-19 22:04:01 +0000

    use pymysql

--- create-database.py
+++ create-database.py
@@ -3,7 +3,7 @@
 import os
 import getpass
 import lib.vamtam as vamtam
-import MySQLdb as MS
+import pymysql
 from colorama import Fore, Back, Style
 
 print("Create database", "")
@@ -13,18 +13,17 @@ mysql_pass = getpass.getpass("%s password:" % mysql_user)
 mysql_database = vamtam.option("Enter new database name")
 
 if(vamtam.confirm('Creating database %s with user %s' % (mysql_database, mysql_user), True)):
-	db1 = MS.connect(host="localhost",user=mysql_user,passwd=mysql_pass)
+	db1 = pymysql.connect(host='localhost', unix_socket='/var/run/mysqld/mysqld.sock', user=mysql_user, passwd=mysql_pass)
 	cursor = db1.cursor()
-	sql = 'CREATE DATABASE %s'
+	sql = 'CREATE DATABASE %s' % mysql_database
 	cursor.execute(sql)
 
 	if(vamtam.confirm('Create new database user?', True)):
 		new_user = vamtam.option("Username", True, mysql_database)
 		new_pass = vamtam.option("Password", True, vamtam.password_generator(25))
 
-		sql = "grant all on %(db)s.* to %(user)s identified by %(pass)s"
+		sql = "grant all on "+mysql_database+".* to %(user)s identified by %(pass)s"
 		cursor.execute(sql, {
-			'db': mysql_database,
 			'user': new_user,
 			'pass': new_pass
 		});
@@ -32,8 +31,8 @@ if(vamtam.confirm('Creating database %s with user %s' % (mysql_database, mysql_u
 		cursor.execute('flush privileges');
 
 		print("")
-		print('Created database '+Fore.RED+mysql_database++Style.RESET_ALL)
-		print('Access with user '+Fore.RED+new_user+Style.RESET_ALL' and '+Fore.RED+new_pass+Style.RESET_ALL)
+		print('Created database '+Fore.RED+mysql_database+Style.RESET_ALL)
+		print('Access with user '+Fore.RED+new_user+Style.RESET_ALL+' and '+Fore.RED+new_pass+Style.RESET_ALL)
 	else:
 		print('Created database '+Fore.RED+mysql_database+Style.RESET_ALL)
 		exit(0)
